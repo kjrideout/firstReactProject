ca2b19fe76f2f5c9bd54659b64d6e9c3f44da538
KJ Rideout <kjrideout@gmail.com>
2021-06-20 05:55:21 -0400

    wk2 Workshop

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class CampsiteInfo extends Component {
+  
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+  
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map(comments => 
+            <div key={comments.id}>
+              {comments.text}<br />
+              --{comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}<br />
+              <br />
+            </div>)}
+        </div>
+      );
+    }
+    return <div />;
+  }
+
+  render() {
+    if (this.props.campsite) {
+      return (
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+}
+
+export default CampsiteInfo;
